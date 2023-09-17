1aa3cb6af404354a0681a69a48cfa8e977ef87d7
Lukas Pistrol <lukas@pistrol.com>
2023-09-17 02:27:44 +0200

    add missing platforms

--- Package.swift
+++ Package.swift
@@ -6,7 +6,11 @@ import PackageDescription
 let package = Package(
     name: "TaskTrigger",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v15),
+        .macOS(.v12),
+        .watchOS(.v8),
+        .tvOS(.v15),
+        .macCatalyst(.v15),
     ],
     products: [
         .library(
