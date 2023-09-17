92ac7b13854d46c951f12ecdd49d3ea77104ee2d
Lukas Pistrol <lukas@pistrol.com>
2023-09-17 10:13:30 +0200

    add build actions

---
+++ .github/scripts/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export LC_ALL=en_US.UTF-8
+
+SCHEME="TaskTrigger"
+
+# Get the destination from the first argument
+DESTINATION=$1
+
+if [ -z "$DESTINATION" ]; then
+    echo "Missing argument: destination"
+    echo "Usage: $0 <destination>"
+    echo "destination: [iOS|macOS|watchOS|tvOS]"
+    exit 1
+fi
+
+set -o pipefail && xcodebuild clean build -scheme $SCHEME \
+    -destination "generic/platform=$DESTINATION" | xcpretty
